777b3a234ce10cc6f44c81165eaafec4e82ac31c
Jack Ngai <jackngai@gmail.com>
2017-05-29 16:30:43 -0400

    feat: Added constants and variable
    Added constants and variable in ViewDidLoad.
    Added call to import SwiftyPlistManager in ViewController.swift.

--- SPMTutorial/ViewController.swift
+++ SPMTutorial/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import SwiftyPlistManager
 
 class ViewController: UIViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        let dataPlistName = "Data"
+        let otherDataPlistname = "OtherData"
+        let nonExistentPlistName = "NonExistent"
+        
+        let newKey = "newKey"
+        let firstKey = "firstKey"
+        let secondKey = "secondKey"
+        let thirdKey = "thirdKey"
+        let forthKey = "forthKey"
+        let nonExistentKey = "nonExistentKey"
+        
+        let helloNewValue = "Hello New Value"
+        let rebeloperValue = "Rebeloper"
+        let intValue = 17
+        let boolValue = true
+        let anotherIntValue = 28
+        let stringValue = "Alex"
+        var dict = ["name": "John", "age":34] as [String : Any]
+        
+
     }
 
     override func didReceiveMemoryWarning() {
